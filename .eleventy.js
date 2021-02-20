f16a0592fe945529442fd8faca0b9da662e754c2
cokkto <cocteau@live.fi>
2021-02-20 15:41:53 +0200

    playing with meta

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const htmlmin = require("html-minifier");
+const htmlEntities = require("html-entities");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
@@ -37,6 +38,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("min", (...numbers) => {
     return Math.min.apply(null, numbers);
+  });
+
+  eleventyConfig.addFilter("entitiesDecode", (str) => {
+    return htmlEntities.decode(str, { level: "html5" });
   });
 
   eleventyConfig.addCollection("tagList", function (collection) {
