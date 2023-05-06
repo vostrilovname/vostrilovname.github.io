c235f8253d5dc3b78b9afecc718128f90e265eea
cokkto <cokkto@users.noreply.github.com>
2023-05-06 18:02:54 +0300

    tina cms

---
+++ tina/config.ts
@@ -0,0 +1,122 @@
+import dayjs from "dayjs";
+import { defineConfig } from "tinacms";
+import { StoriesFields, SpecialFields, CinemaFields, LibraryFields } from "./templates";
+
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "c0d519f8-2ec4-4d54-a412-e4c9360176a8", // Get this from tina.io
+  token: "4fbc1c00f3d2cbce62926b4998b080bcf157f269", // Get this from tina.io
+  client: { skip: true },
+  build: {
+    outputFolder: "ed",
+    publicFolder: "_site",
+  },
+  media: {
+    tina: {
+      mediaRoot: "/assets/uploads",
+      publicFolder: "_site",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Рассказ",
+        name: "stories",
+        path: "/src/stories",
+        match: {
+          include: "**/*",
+          exclude: "post",
+        },
+        fields: [
+          ...StoriesFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Кинозал",
+        name: "cinema",
+        path: "/src/cinema",
+        match: {
+          include: "**/*",
+          exclude: "cinema",
+        },
+        fields: [
+          ...CinemaFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Разное",
+        name: "special",
+        path: "/src/special",
+        match: {
+          include: "**/*",
+          exclude: "special",
+        },
+        fields: [
+          ...SpecialFields(),
+        ],
+
+        defaultItem: () => {
+          return {
+            // When a new post is created the title field will be set to "New post"
+            date: dayjs().toString(),
+          }
+        }
+      },
+      {
+        format: "md",
+        label: "Библиотека",
+        name: "library",
+        path: "/src/library",
+        match: {
+          include: "**/*",
+          exclude: "library",
+        },
+        fields: [
+          ...LibraryFields(),
+        ],
+
+        defaultItem: () => {
+          return {
+            // When a new post is created the title field will be set to "New post"
+            eleventyNavigation: {
+              parent: "Библиотека",
+              order: 1,
+              key: ""
+            },
+          }
+        },
+      },
+      {
+        format: "md",
+        label: "Друзья семьи",
+        name: "friends",
+        path: "/src/friends",
+        match: {
+          include: "**/*",
+          exclude: "friends",
+        },
+        fields: [
+          ...LibraryFields(),
+        ],
+
+        defaultItem: () => {
+          return {
+            // When a new post is created the title field will be set to "New post"
+            eleventyNavigation: {
+              parent: "История семьи",
+              order: 1,
+              key: ""
+            },
+          }
+        },
+      },
+    ],
+  },
+});
